93a0df2ba3325798371c3bffc411d1b876652085
OpenPlugin <14420465+openplugin@user.noreply.gitee.com>
2024-11-18 11:28:19 +0800

    add checkbox

---
+++ BST.cs
@@ -0,0 +1,35 @@
+﻿namespace TidyWin32;
+public partial class Win32
+{
+    /// <summary>
+    /// 复选框未选中状态
+    /// </summary>
+    public const int BST_UNCHECKED = 0x0000;
+
+    /// <summary>
+    /// 复选框选中状态
+    /// </summary>
+    public const int BST_CHECKED = 0x0001;
+
+    /// <summary>
+    /// 复选框三态模式下的不确定状态
+    /// （仅适用于具有 BS_AUTO3STATE 或 BS_3STATE 样式的复选框）
+    /// </summary>
+    public const int BST_INDETERMINATE = 0x0002;
+
+    /// <summary>
+    /// 当复选框处于未选中状态时，表示该状态获得焦点
+    /// （通常与键盘交互相关）
+    /// </summary>
+    public const int BST_FOCUS = 0x0008;
+
+    /// <summary>
+    /// 当复选框处于选中状态时，表示该状态获得焦点
+    /// </summary>
+    public const int BST_CHECKED_FOCUS = BST_CHECKED | BST_FOCUS;
+
+    /// <summary>
+    /// 当复选框处于不确定状态时，表示该状态获得焦点
+    /// </summary>
+    public const int BST_INDETERMINATE_FOCUS = BST_INDETERMINATE | BST_FOCUS;
+}
---
+++ Windows/CheckBoxInterface.cs
@@ -0,0 +1,41 @@
+﻿using TidyHPC.LiteJson;
+
+namespace TidyWin32;
+
+public partial class Win32
+{
+    public class CheckBoxInterface(Json target) : WindowInterface(target)
+    {
+        public static implicit operator CheckBoxInterface(Json target) => new CheckBoxInterface(target);
+
+        public static implicit operator Json(CheckBoxInterface comboBox) => comboBox.Target;
+
+        public static implicit operator CheckBoxInterface(IntPtr hWnd) => New(hWnd);
+
+        public new static CheckBoxInterface New()
+        {
+            return Json.NewObject();
+        }
+
+        public new static CheckBoxInterface New(IntPtr hWnd)
+        {
+            CheckBoxInterface result = Json.NewObject();
+            result.hWnd = hWnd;
+            return result;
+        }
+
+        public bool Checked
+        {
+            get
+            {
+                var result = SendMessage(hWnd, BM_GETCHECK, 0, 0) == BST_CHECKED;
+                Target.Set(nameof(Checked), result);
+                return result;
+            }
+            set
+            {
+                SendMessage(hWnd, BM_SETCHECK, value ? BST_CHECKED : BST_UNCHECKED, 0);
+            }
+        }
+    }
+}
